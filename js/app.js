8a94c4b2f587973f8dcd2e142961f51dcf02059b
jamesbaptist451-arch <jamesbaptist451@gmail.com>
2026-01-02 11:08:07 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { analizarNoticias } from "../modules/newsEngine.js";
 document.addEventListener("DOMContentLoaded", () => {
  const decisionNoticias = document.getElementById("decision-noticias");
-
+}
 if (decisionNoticias) {
   const resultado = analizarNoticias();
   decisionNoticias.textContent = resultado.texto;
